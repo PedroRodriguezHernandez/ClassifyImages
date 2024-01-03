a76ad3eeab3bf1591198f6c8fe78dd55fed511f1
Pedro Manuel <pedrom.rodriguezhernandez@gmail.com>
2024-01-03 19:26:12 +0000

    Thirst Commit

--- run.py
+++ run.py
@@ -2,37 +2,42 @@ from classify import move_resize_images
 
 
 # Define the original folders
-car_folder = 'C:/Users/Propietario/Desktop/Dataset/Car'
-truck_folder = 'C:/Users/Propietario/Desktop/Dataset/Truck'
-motorbike_folder = 'C:/Users/Propietario/Desktop/Dataset/Motorcycle'
-bus_folder = 'C:/Users/Propietario/Desktop/Dataset/Bus'
-van_folder = 'C:/Users/Propietario/Desktop/Dataset/Van'
+main_folder = 'C:/Users/Propietario/Desktop/images'
+bus_folder = main_folder + '/bus'
+car_folder = main_folder + '/car'
+motorcycle_folder = main_folder + '/motorcycle'
+trains_folder = main_folder + '/train'
+truck_folder = main_folder + '/truck'
+
 # Create destination folders
-train_folder = 'C:/Users/Propietario/Desktop/DatasetTypeVehicle/train'
-validation_folder = 'C:/Users/Propietario/Desktop/DatasetTypeVehicle/validation'
-test_folder = 'C:/Users/Propietario/Desktop/DatasetTypeVehicle/test'
+train_folder = 'C:/Users/Propietario/Desktop/TypesVehicles/train'
+validation_folder = 'C:/Users/Propietario/Desktop/TypesVehicles/validation'
+test_folder = 'C:/Users/Propietario/Desktop/TypesVehicles/test'
 
 # Percentages
 train_percentage = 0.7
 validation_percentage = 0.2
 test_percentage = 0.1
-new_size = (200,200)
+new_size = (150, 150)
 
 print("Start train image............................\n ")
-move_resize_images(car_folder, train_folder + '/Car', train_percentage, new_size)
-move_resize_images(truck_folder, train_folder + '/Truck', train_percentage, new_size)
-move_resize_images(motorbike_folder, train_folder + '/Motorbike', train_percentage, new_size)
-move_resize_images(bus_folder, train_folder + '/Bus', train_percentage, new_size)
-move_resize_images(van_folder, train_folder + '/Van', train_percentage, new_size)
+
+move_resize_images(bus_folder, train_folder + '/bus' , train_percentage, new_size)
+move_resize_images(car_folder, train_folder + '/car' , train_percentage, new_size)
+move_resize_images(motorcycle_folder, train_folder + '/motorcycle', train_percentage, new_size)
+move_resize_images(trains_folder, train_folder + '/train', train_percentage, new_size)
+move_resize_images(truck_folder, train_folder + '/truck', train_percentage, new_size)
+
 print("Start validation image............................\n ")
-move_resize_images(car_folder, validation_folder + '/Car', validation_percentage, new_size)
-move_resize_images(truck_folder, validation_folder + '/Truck', validation_percentage, new_size)
-move_resize_images(motorbike_folder, validation_folder + '/Motorbike', validation_percentage, new_size)
-move_resize_images(bus_folder, validation_folder + '/Bus', validation_percentage, new_size)
-move_resize_images(van_folder, validation_folder + '/Van', validation_percentage, new_size)
+move_resize_images(bus_folder, validation_folder + '/bus' , validation_percentage, new_size)
+move_resize_images(car_folder, validation_folder + '/car' , validation_percentage, new_size)
+move_resize_images(motorcycle_folder, validation_folder + '/motorcycle', validation_percentage, new_size)
+move_resize_images(trains_folder, validation_folder + '/train', validation_percentage, new_size)
+move_resize_images(truck_folder, validation_folder + '/truck', validation_percentage, new_size)
 print("Start test image............................\n ")
-move_resize_images(car_folder, test_folder + '/Car', test_percentage, new_size)
-move_resize_images(truck_folder, test_folder + '/Truck', test_percentage, new_size)
-move_resize_images(motorbike_folder, test_folder + '/Motorbike', test_percentage, new_size)
-move_resize_images(bus_folder, test_folder + '/Bus', test_percentage, new_size)
-move_resize_images(van_folder, test_folder + '/Van', test_percentage, new_size)
+
+move_resize_images(bus_folder, test_folder + '/bus' , test_percentage, new_size)
+move_resize_images(car_folder, test_folder + '/car' , test_percentage, new_size)
+move_resize_images(motorcycle_folder, test_folder + '/motorcycle', test_percentage, new_size)
+move_resize_images(trains_folder, test_folder + '/train', test_percentage, new_size)
+move_resize_images(truck_folder, test_folder + '/truck', test_percentage, new_size)
